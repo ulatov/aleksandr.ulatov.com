93bd21ba660d418086dde207e6c8019ad29b902d
Aleksandr Ulatov <aleksandr@ulatov.name>
2020-07-29 15:12:56 +0500

    well done one fcc project

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,23 @@
+/*
 module.exports = {
     dir: {
       input: "dev-site",
       output: "docs"
     }
   };
+*/
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.setTemplateFormats([
+    "jpg",
+    "css",  // css is not yet a recognized template extension in Eleventy
+    "CNAME"
+  ]);
+  return {
+    dir: {
+      input: "dev-site",
+      layouts: "dev-site/_includes",
+      output: "docs"
+    }
+  };
+};
